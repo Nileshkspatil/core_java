3a6b9a2327d775788739ed1cb774e65b6a3f4c4d
Nileshkspatil <nileshkspatil@gmail.com>
2024-09-09 12:46:38 +0530

    changes

--- corejava/src/collection/P1.java
+++ corejava/src/collection/P1.java
@@ -7,6 +7,7 @@ public class P1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello");
 		List l=new ArrayList<>(); 
 		l.add(99);
 		l.add("nilesh");
